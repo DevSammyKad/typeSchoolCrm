e747a0dc1e2b0ee033ac234575a6c459b034bcad
DevSammyKad <DevSammyKad@gmail.com>
2025-01-10 15:23:01 +0530

    fix node modules

--- src/app/dashboard/attendance/page.tsx
+++ src/app/dashboard/attendance/page.tsx
@@ -1,7 +1,7 @@
 import AttendanceClientPage from '@/app/components/dashboard/AttendanceClientPage';
 import React from 'react';
 
-export const runtime = 'edge';
+// export const runtime = 'edge';
 
 export const revalidate = 0;
 
